119348a1fe4c000d538b9308054db8ca7bcab57a
Mike Herrera <mike.herrera@openx.com>
2020-01-22 11:34:09 -0800

    updated sys path for update schema

--- bin/updateSchema.py
+++ bin/updateSchema.py
@@ -1,19 +1,27 @@
 """this would backup schema tables and views"""
-
 import os
+import sys
+
+PROJ_DIR = os.path.dirname(os.path.abspath(os.path.join(__file__, '..', '..')))
+sys.path.append(PROJ_DIR)
+
 import datetime
 import yaml
 import subprocess
 import shutil
 import pandas as pd
 from mikesnowflake.access.snowFlakeAccess import SnowFlakeAccess
-from util.yamlUtil import getYamlDependencies
+from mikesnowflake.util.yamlUtil import getYamlDependencies
 
 os.nice(20)
 
 
 def main(user, password):
-    """
+    """the main entry point to the script
+
+    Args:
+        user(str): snowflake username
+        password(str): snowflake password
     """
     sfa = SnowFlakeAccess(user, password)
 
@@ -30,12 +38,12 @@ def main(user, password):
     # backup yaml config dependencies
     newYamlFile = os.path.join(fileDir, 'yaml_%s.csv' % td.strftime('%Y%m%d%H%M'))
     cmd = 'cp %s %s' % (yamlFile, newYamlFile)
-    #shutil.copyfile(yamlFile, newYamlFile)
+    shutil.copyfile(yamlFile, newYamlFile)
     print("copied %s to %s" % (yamlFile, newYamlFile))
 
     workSpace='/Users/mike.herrera/workspace/'
     df = getYamlDependencies(workspace)
-    #df.to_csv(yamlFile, sep='|')
+    df.to_csv(yamlFile, sep='|')
     print('written yaml dependency to %s' % yamlFile)
 
 
